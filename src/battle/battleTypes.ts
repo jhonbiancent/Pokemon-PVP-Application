d43636e2ea44c0d201f837f6be27a02345389e00
jhonbiancent <jhonbiancent06@gmail.com>
2026-04-19 15:44:09 +0800

    feat: created battle types

---
+++ src/battle/battleTypes.ts
@@ -0,0 +1,9 @@
+import { Pokemon } from "../types/pokemon";
+
+export type BattleState = {
+  player: Pokemon;
+  enemy: Pokemon;
+  turn: "player" | "enemy";
+  log: string[];
+  winner: "player" | "enemy" | null;
+};
